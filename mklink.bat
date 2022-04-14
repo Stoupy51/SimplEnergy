006c8eb746f2c94e093e2636592f2720f37b792f
edayot <pro.e.dayot@gmail.com>
2022-04-14 17:20:47 +0200

    Add mklink.bat to follow changes on resource pack

---
+++ mklink.bat
@@ -0,0 +1,7 @@
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\saves\Datapack island\datapacks\SimplEnergy DataPack\"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\resourcepacks\SimplEnergy ResourcePack"
+
+
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\saves\Datapack island\datapacks\SimplEnergy DataPack" "C:\Users\erwan\Documents\Dev\SimplEnergy-1"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\resourcepacks\SimplEnergy ResourcePack" "C:\Users\erwan\Documents\Dev\SimplEnergy-1\resource pack"
+PAUSE
