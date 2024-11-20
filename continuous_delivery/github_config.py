9e42d8dfa144a9ac217f20fbb89e68660cc6a316
Stoupy51 <stoupy51@gmail.com>
2024-11-20 22:55:27 +0100

    feat: ✨ Added github CD

---
+++ continuous_delivery/github_config.py
@@ -0,0 +1,11 @@
+
+# Configuration for GitHub
+from config import *
+
+# Configuration
+github_config: dict = {
+	"project_name": PROJECT_NAME,
+	"version": VERSION,
+	"build_folder": BUILD_FOLDER,
+}
+
--- upload.py
+++ upload.py
@@ -1,12 +1,16 @@
 
 # Imports
-from python_datapack.continuous_delivery import load_credentials, upload_to_modrinth
+from python_datapack.continuous_delivery import load_credentials, upload_to_github, upload_to_modrinth
 
 # Get credentials
 credentials: dict[str, str] = load_credentials("~/python_datapack/credentials.yml")
 
 ## Uploads
+# Upload to GitHub
+from continuous_delivery.github_config import github_config
+changelog: str = upload_to_github(credentials, github_config)
+
 # Upload to Modrinth
 from continuous_delivery.modrinth_config import modrinth_config
-upload_to_modrinth(credentials, modrinth_config)
+upload_to_modrinth(credentials, modrinth_config, changelog)
 
