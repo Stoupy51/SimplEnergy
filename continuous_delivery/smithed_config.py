a027b52f872bfa282f1e5e3bd2a0d6af2e9bfa10
Stoupy51 <stoupy51@gmail.com>
2024-11-23 21:43:33 +0100

    feat: ✨ Smithed CD support

---
+++ continuous_delivery/smithed_config.py
@@ -0,0 +1,11 @@
+
+# Configuration for Smithed
+from config import *
+
+# Configuration
+smithed_config: dict = {
+	"project_id": NAMESPACE,
+	"project_name": PROJECT_NAME,
+	"version": VERSION,
+}
+
