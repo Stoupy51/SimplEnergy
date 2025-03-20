5313563a96309993688598a4e0598028c498e309
Stoupy51 <stoupy51@gmail.com>
2025-03-20 14:38:34 +0100

    refactor: ♻️ Enhanced function writing and namespace handling in rotatable setup

--- user/utils/rotatable.py
+++ user/utils/rotatable.py
@@ -5,30 +5,30 @@ from python_datapack.utils.io import *
 
 # Setup rotatables tags
 def setup_rotatable_tags(config: dict, rotatables: list[str]) -> None:
-	namespace: str = config['namespace']
-	build_datapack: str = config['build_datapack']
-	path: str = f"{build_datapack}/data/{namespace}/function/custom_blocks"
+	namespace: str = config["namespace"]
 
 	# Add tags for rotatables
 	for rotatable in rotatables:
-		file: str = f"{path}/{rotatable}/place_secondary.mcfunction"
-		write_to_file(file, f"\n# Make the block rotatable by wrench\ntag @s add {namespace}.rotatable")
+		write_to_function(config,
+			f"{namespace}:custom_blocks/{rotatable}/place_secondary",
+			f"\n# Make the block rotatable by wrench\ntag @s add {namespace}.rotatable"
+		)
 
 
 	## Link with mechanization wrench
-	MECH_FUNCTION_TAG: str = f"{build_datapack}/data/mechanization/tags/function"
-	MECH_CALLS: str = f"{build_datapack}/data/{namespace}/function/calls/mechanization"
-
 	# Link function tags
-	json_content: dict = {"required":False,"values":[f"{namespace}:calls/mechanization/wrench_break"]}
-	write_to_file(f"{MECH_FUNCTION_TAG}/wrench_break.json", stp.super_json_dump(json_content))
-	json_content = {"required":False,"values":[f"{namespace}:calls/mechanization/wrench_modify"]}
-	write_to_file(f"{MECH_FUNCTION_TAG}/wrench_modify.json", stp.super_json_dump(json_content))
+	json_content: dict[str, bool | list[str]] = {"required": False, "values": [f"{namespace}:calls/mechanization/wrench_break"]}
+	write_to_tags(config, "mechanization:function/wrench_break", stp.super_json_dump(json_content))
+	json_content = {"required": False, "values": [f"{namespace}:calls/mechanization/wrench_modify"]}
+	write_to_tags(config, "mechanization:function/wrench_modify", stp.super_json_dump(json_content))
 
 	# Write slots functions
-	write_to_file(f"{MECH_CALLS}/wrench_break.mcfunction", f"""
+	write_to_function(config, f"{namespace}:calls/mechanization/wrench_break", f"""
 execute if entity @s[tag={namespace}.custom_block] run setblock ~ ~ ~ air destroy
 execute if entity @s[tag={namespace}.custom_block] run function {namespace}:custom_blocks/destroy
 """)
-	write_to_file(f"{MECH_CALLS}/wrench_modify.mcfunction", f"execute if entity @s[tag={namespace}.rotatable] run function {namespace}:utils/wrench/rotate")
+	write_to_function(config,
+		f"{namespace}:calls/mechanization/wrench_modify",
+		f"execute if entity @s[tag={namespace}.rotatable] run function {namespace}:utils/wrench/rotate"
+	)
 
