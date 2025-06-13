4ecdd078b05c7873208b67c48cb53ee0bba970c2
Stoupy51 <stoupy51@gmail.com>
2025-06-13 20:44:05 +0200

    fix: 🐛 Updated continuous delivery stuff to stewbeet

--- continuous_delivery/pmc_config.py
+++ continuous_delivery/pmc_config.py
@@ -1,6 +1,10 @@
 
-# Configuration for PlanetMinecraft
-from config import *
+# Imports
+from beet import ProjectConfig
+from stewbeet.utils import get_project_config
+
+# Try to find and load the beet configuration file
+cfg: ProjectConfig | None = get_project_config()
 
 # Constants
 PMC_URL: str = "https://www.planetminecraft.com/account/manage/data-packs/5305840/"
@@ -8,6 +12,6 @@ PMC_URL: str = "https://www.planetminecraft.com/account/manage/data-packs/530584
 # Configuration
 pmc_config: dict = {
 	"project_url": PMC_URL,
-	"version": VERSION,
+	"version": cfg.version,
 }
 
