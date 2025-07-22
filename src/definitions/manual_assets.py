f15512d4b33cad8c49819fa7a28a7d03802736b9
Stoupy51 <stoupy51@gmail.com>
2025-07-22 22:55:27 +0200

    wip: 🚧 Adding basic item cable

--- src/definitions/manual_assets.py
+++ src/definitions/manual_assets.py
@@ -17,6 +17,7 @@ def manual_assets_main() -> None:
 	super_copy(f"{ASSETS_FOLDER}/simple_cable.png", f"{manual_cache}/items/{Mem.ctx.project_id}/")
 	super_copy(f"{ASSETS_FOLDER}/advanced_cable.png", f"{manual_cache}/items/{Mem.ctx.project_id}/")
 	super_copy(f"{ASSETS_FOLDER}/elite_cable.png", f"{manual_cache}/items/{Mem.ctx.project_id}/")
+	super_copy(f"{ASSETS_FOLDER}/basic_item_cable.png", f"{manual_cache}/items/{Mem.ctx.project_id}/")
 
 	return
 
